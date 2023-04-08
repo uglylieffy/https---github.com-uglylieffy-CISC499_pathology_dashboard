3eae28777534af86bd2ec843f235a33e7d2e285e
uglylieffy <71037563+uglylieffy@users.noreply.github.com>
2023-04-08 13:36:00 -0400

    modified:   src/components/AppHeader.js

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,11 +1,9 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
 import {
   CContainer,
   CHeader,
   CHeaderBrand,
-  CHeaderDivider,
   CHeaderNav,
   CNavItem,
 } from '@coreui/react'
@@ -13,8 +11,6 @@ import CIcon from '@coreui/icons-react'
 import { logo } from 'src/assets/brand/logo'
 
 const AppHeader = () => {
-  const dispatch = useDispatch()
-  const sidebarShow = useSelector((state) => state.sidebarShow)
 
   return (
     <CHeader position="sticky" className="mb-4">
